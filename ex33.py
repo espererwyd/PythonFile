38f3b2a18721748631beed2903af650695807dc0
espererwyd <espererwyd@gmail.com>
2017-10-23 20:10:47 +0800

    till ex35

--- ex33.py
+++ ex33.py
@@ -1,22 +1,42 @@
 i = 0
 numbers = []
-range_condition = (0,6)
+max_range = 6
 increment = int(input("> Please input your ideal increment here: "))
 
-def append_numbers_to_list(i):
-    while i < 6:
-        print(f"At the top i is {i}")
+# def append_numbers_to_list(i):
+#     while i < 6:
+#         print(f"At the top i is {i}")
+#         numbers.append(i)
+#
+#         i += increment
+#         print("Numbers now: ", numbers)
+#         print(f"At the bottom i is {i}")
+#
+# append_numbers_to_list(i)
+#
+# print("The numbers:")
+#
+# for n in numbers:
+#     print(n)
+#
+# print("So the list is: ", numbers)
+
+def apppend_numbers_to_list(i):
+    for i in range(0, max_range):
+        print(f"At the top i is: {i}")
         numbers.append(i)
 
-        i += increment
-        print("Numbers now: ", numbers)
+        print("Numbers now:", numbers)
         print(f"At the bottom i is {i}")
 
-append_numbers_to_list(i)
+apppend_numbers_to_list(i)
 
 print("The numbers:")
 
-for n in numbers:
-    print(n)
+for i in numbers:
+    print(i)
 
 print("So the list is: ", numbers)
+
+# A for-loop can only iterate (loop) over collections of things.
+# A while-loop can do any kind of iteration (looping) you want
---
+++ ex34.py
@@ -0,0 +1,8 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+
+first_animal = animals[0]
+second_animal = animals[1]
+third_animal = animals[2]
+fourth_animal = animals[3]
+
+print(first_animal, second_animal, third_animal, fourth_animal)
